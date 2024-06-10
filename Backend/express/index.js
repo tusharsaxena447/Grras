cee812e23c0e632bfdb301b2b95e3b6b149dc05d
tusharsaxena447 <65817532+tusharsaxena447@users.noreply.github.com>
2024-06-10 19:42:08 +0530

    middleware

--- Backend/express/index.js
+++ Backend/express/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use((req,res,next)=>{
+    console.log("middleware running")
+    next()
+})
+
 app.get('/', (req,res)=>{
     res.send("this is home page")
 })
